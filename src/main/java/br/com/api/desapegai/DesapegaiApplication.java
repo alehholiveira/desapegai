87dbe76e12a90398880d9ddb0bcb40b06537364c
Alexandre <aletoscaro@gmail.com>
2023-10-19 20:30:54 -0300

    configuração springboot

---
+++ src/main/java/br/com/api/desapegai/DesapegaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.desapegai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesapegaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesapegaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/desapegai/DesapegaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.desapegai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesapegaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
