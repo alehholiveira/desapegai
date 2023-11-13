98a2f525dd8e86cfb11b81b28162aad278658219
Alexandre <aletoscaro@gmail.com>
2023-11-13 14:36:37 -0300

    corrigindo login

--- src/main/java/br/com/api/desapegai/Controller/AdController.java
+++ src/main/java/br/com/api/desapegai/Controller/AdController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import br.com.api.desapegai.Model.Ad;
-import br.com.api.desapegai.Model.User;
 import br.com.api.desapegai.Service.AdService;
 
 
