1152eefe7fcf72d8aa85a8509f9f71c71ee0b1cc
Alexandre <aletoscaro@gmail.com>
2023-11-28 01:48:09 -0300

    visual

--- src/main/java/br/com/api/desapegai/Controller/GoogleMapsController.java
+++ src/main/java/br/com/api/desapegai/Controller/GoogleMapsController.java
@@ -1,6 +1,5 @@
 package br.com.api.desapegai.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ public class GoogleMapsController {
 
     private final GoogleMapsService googleMapsService;
 
-    @Autowired
     public GoogleMapsController(GoogleMapsService googleMapsService) {
         this.googleMapsService = googleMapsService;
     }
